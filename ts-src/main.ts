bbfbdb88f93322a85ba29753152cbc8b825cf6c1
sandwools <arktis@foxmail.com>
2022-12-16 19:23:01 +0800

    完成

--- ts-src/main.ts
+++ ts-src/main.ts
@@ -1,3 +1,4 @@
+import { time } from 'console';
 import { GreedyMod,DynamicMod, BranchGaugeMod,Backtracking } from './Computer';
 import { getC,getP,getW,getS,getCPSW,writeAns} from './IO';
 
@@ -9,14 +10,21 @@ for(let i=0;i<3;i++)
     let {c,p,s,w}=getCPSW(BASE_PATH+DIR_NUM[i]);
     console.log("p:"+p);
     console.log("w:"+w);
+    let t0 = new Date().getTime();
     let ans1 = GreedyMod(c,p,s,w,i);
+    console.log("time: "+(()=>{let t1= new Date().getTime(); return t1-t0;})())
     console.log("-------");
     let ans2 = DynamicMod(c,p,s,w,i);
+    console.log("time: "+(()=>{let t2= new Date().getTime(); return t2-t0;})())
     console.log("-------");
     let ans3 = BranchGaugeMod(c,p,s,w,i);
+    console.log("time: "+(()=>{let t3= new Date().getTime(); return t3-t0;})())
     console.log("-------");
     let ans4 = Backtracking(c,p,s,w,i);
+    console.log("time: "+(()=>{let t4= new Date().getTime(); return t4-t0;})())
 
     writeAns(i.toString(),[ans1,ans2,ans3,ans4]);
 }
+
+debugger;
 
